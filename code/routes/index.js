697b706fb96e7caf157b02cc35bce2813104b489
Faisal Umar <Faisal_umar_azhar@hotmail.com>
2024-05-17 05:06:12 +0000

    Idk unsaved changes, I think it's an error

--- code/routes/index.js
+++ code/routes/index.js
@@ -15,12 +15,24 @@ router.get('/landingpage', function(req, res, next){
   res.sendFile(path.join(__dirname, '../public/homepage.html'));
 });
 
-router.get('/branch*', function(req, res, next){
+router.get('/branch', function(req, res, next){
   res.sendFile(path.join(__dirname, '../public/branch.html'));
+});
+
+router.get('/joinBranch', function(req, res, next){
+  res.sendFile(path.join(__dirname, '../public/joinBranch.html'));
 });
 
 router.get('/default', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/default.html'));
+});
+
+router.get('/test', function(req, res, next){
+  res.send("Hello World2!");
+});
+
+router.get('/testLogIn', function(req, res, next){
+  res.send(true);
 });
 
 module.exports = router;
