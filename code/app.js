34c42e7fdedcb160cf4ef014c7b8643f5e1b4344
ManavBijlani21 <119971901+ManavBijlani@users.noreply.github.com>
2024-06-01 16:13:25 +0000

    Completed the implementation of the tables and a database for the website

--- code/app.js
+++ code/app.js
@@ -3,10 +3,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Use mysql in this app
+var mysql = require('mysql');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Create a pool (group) of connections to be used for connecting with SQL server
+var dbConnectionPool = mysql.createPool({
+    host : 'localhost', //Location of database
+    database : 'Website_Database'
+})
+
 var app = express();
+
+//Express will run this function on every request
+//For all requests that we handle in index.js, we'll be able to access the pool of connections using req.pool
+app.use(function(req,res,next){
+    req.pool = dbConnectionPool; //pool value can be changed if needed.
+    next();
+
+})
 
 app.use(logger('dev'));
 app.use(express.json());
