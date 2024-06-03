a9c7a54c04a8f28badeda4a92e98e0fc96cf597d
Jack Rowlands <119843072+jackdrowlands@users.noreply.github.com>
2024-06-03 17:28:42 +0930

    feat: verification check

--- code/routes/users.js
+++ code/routes/users.js
@@ -4,8 +4,20 @@ var router = express.Router();
 router.use(function (req, res, next) {
   /* Run the check to see if user logged in */
   // there is a token in the header
-  if (req.headers.authToken === "test") {
-    next();
+  const token = req.headers['authorization'];
+
+  if (token) {
+    // Verify the token
+    jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+      if (err) {
+        // If verification fails, send an unauthorized response
+        return res.status(401).json({ message: "Unauthorized" });
+      } else {
+        // If verification succeeds, proceed to the next middleware or route handler
+        req.decoded = decoded;
+        next();
+      }
+    });
   }
   else {
     res.status(401).json({ message: "Unauthorized" });
